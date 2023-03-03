7b32464c06a0bc4b4d0acf0c3d08b62e52e38f24
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-03 22:49:09 +0600

    1. ejs setup

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  if (req.method === 'GET') {
-    res.status(201).send({
-      message: 'Welcome to the ejs chapters',
-    });
-  }
+  res.render('index');
 });
 
 app.listen(PORT, () => {
