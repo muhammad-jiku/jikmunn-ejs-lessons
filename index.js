b194722b721e8a26fc8db9ad8e798f73296eb123
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-03 22:43:36 +0600

    express server setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Welcome to ejs tutorial');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  if (req.method === 'GET') {
+    res.status(201).send({
+      message: 'Welcome to the ejs chapters',
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
